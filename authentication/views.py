b6a5b71b470fa4287f07ce189aa094828bf39eb5
Ray Lv <ray.lv@sap.com>
2017-06-27 16:46:16 +0800

    ray

---
+++ authentication/views.py
@@ -0,0 +1,29 @@
+import datetime, json, pytz
+from django.http import HttpResponse
+from django.conf import settings
+from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
+
+class ObtainExpiringAuthToken(ObtainAuthToken):
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            attrs = serializer.validate(request.data)
+            token, created = Token.objects.get_or_create(user=attrs['user'])
+
+            utc_now = datetime.datetime.utcnow()
+            if not created and token.created.replace(tzinfo=pytz.utc).replace(
+                    tzinfo=None) < utc_now - datetime.timedelta(seconds=settings.TOKEN_EXPIRATION_TIME):
+                token.delete()
+                token = Token.objects.create(user=attrs['user'])
+                token.created = datetime.datetime.utcnow()
+                token.save()
+            response_data = {'token': token.key}
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+        return HttpResponse(serializer.errors.values(), status=status.HTTP_400_BAD_REQUEST)
+
+
+obtain_expiring_auth_token = ObtainExpiringAuthToken.as_view()
