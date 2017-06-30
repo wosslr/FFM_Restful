3a4ddacd79e300f57b4a764ff3d06b4a45c83388
Ray Lv <ray.lv@sap.com>
2017-06-30 17:21:58 +0800

    ray

--- restui/static/restui/ffm-app/src/App.js
+++ restui/static/restui/ffm-app/src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery/';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super()
+        this.state = {
+            apiData: ""
+        }
+        this.clickGo = this.clickGo.bind(this)
+    }
+
+    clickGo() {
+        $.ajax({
+            url: 'http://localhost:8000/restdemo2'
+        }).done(data => {
+            this.setState({
+                apiData: data
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                    <button type="submit" onClick={this.clickGo}>Go!</button>
+                    <p>{this.state.apiData}</p>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
